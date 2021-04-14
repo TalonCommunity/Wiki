382514b47d42c056d739900e3ca41735f56231ba
Koko <koko@Kokos-Mac-mini.local>
2021-04-13 21:42:59 -0500

    notify.py is a file that is linked from the advanced talon page
    which was deleted in the commit immediately previous to this one.

--- extras/notify.py
+++
@@ -1,7 +0,0 @@
-# This file will add a notification to tell you what Talon heard you say
-from talon import app, speech_system
-def on_phrase(j):
-    phrase = getattr(j["parsed"], "_unmapped", j["phrase"])
-    phrase = " ".join(word.split("\\")[0] for word in phrase)
-    app.notify(phrase)
-speech_system.register('post:phrase', on_phrase)
