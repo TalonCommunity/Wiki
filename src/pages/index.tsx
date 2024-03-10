30c4a34ddd7e78a0c62ecbaa4aac1666cc283055
Colton <70598503+C-Loftus@users.noreply.github.com>
2024-03-10 16:42:23 -0400

    improved wiki metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,8 +27,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Community Wiki for Talon Voice">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
