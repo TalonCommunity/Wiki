ff387704d8a55e3139bbdfd947b3e950701ddc75
Colton <70598503+C-Loftus@users.noreply.github.com>
2024-03-10 16:32:04 -0400

    init wiki redesign

--- .env-files/Gemfile.github
+++
@@ -1,3 +0,0 @@
-source 'http://rubygems.org'
-gem 'github-pages', group: :jekyll_plugins
-gem "jekyll-gitlab-metadata" # for cross compatibility
---
+++ docusaurus.config.ts
@@ -0,0 +1,135 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "Talon Community Wiki",
+  tagline: "Documentation for using Talon Voice",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://talon.wiki",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "Talon Community", // Usually your GitHub org/user name.
+  projectName: "Talo Community Wiki", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          routeBasePath: "/", // Serve the docs at the site's root
+
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+        blog: false, // Optiona
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      logo: {
+        alt: "My Site Logo",
+        src: "img/talon.png",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "IntroSidebar",
+          position: "left",
+          label: "Talon-Intro",
+        },
+        {
+          type: "docSidebar",
+          sidebarId: "ScriptingSidebar",
+          position: "left",
+          label: "Talon-Scripting",
+        },
+        {
+          type: "docSidebar",
+          sidebarId: "IntegrationSidebar",
+          position: "left",
+          label: "Integrations",
+        },
+        {
+          href: "https://github.com/TalonCommunity/Wiki/",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Docs",
+              to: "/docs/",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Official Website",
+              href: "https://talonvoice.com/",
+            },
+            {
+              label: "Slack",
+              href: "https://talonvoice.com/chat",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "GitHub",
+              href: "https://github.com/TalonCommunity/Wiki/",
+            },
+            {
+              label: "Donate",
+              href: "https://www.patreon.com/join/lunixbochs",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Talon Community`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
