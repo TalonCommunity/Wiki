f99f70e5975dd5de8f607dc59bc9b8364abb6614
Colton <70598503+C-Loftus@users.noreply.github.com>
2024-03-20 19:06:56 -0400

    change edit url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -39,8 +39,7 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/TalonCommunity/Wiki",
         },
         theme: {
           customCss: "./src/css/custom.css",
