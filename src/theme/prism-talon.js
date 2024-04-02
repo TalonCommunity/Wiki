d3629c21d97cdc32fbf71f6565515c62c1b43dda
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2024-04-02 15:54:28 -0400

    fix merge again

--- src/theme/prism-talon.js
+++ src/theme/prism-talon.js
@@ -1,7 +1,6 @@
 // bring in source from https://github.com/mrob95/vscode-TalonScript/blob/master/syntaxes/talon.tmLanguage.json
 
 Prism.languages.talon = {
-<<<<<<< HEAD:src/theme/prism-talon.js
   comment: {
     pattern: /(^|[^\\])#.*/,
     lookbehind: true,
@@ -9,8 +8,3 @@ Prism.languages.talon = {
   },
 };
 
-
-=======
-  // TODO
-};
->>>>>>> f35157425c43926e39118377d822a38683aad6c5:src/theme/prism-talon-syntax-highlighter.js
