5e7edf06e534d7644a69b767b92a366a74f5c7b1
Colton <70598503+C-Loftus@users.noreply.github.com>
2024-04-28 13:27:46 -0400

    algolia

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -52,6 +52,31 @@ const config: Config = {
     // Replace with your project's social card
     image: "img/talon-community-logo-social.png",
 
+    algolia: {
+      // The application ID provided by Algolia
+      appId: "H33NOZZCSH",
+
+      // Public API key: it is safe to commit it
+      apiKey: "7eadeff02464a3bfaa8b2341bcd6c834",
+
+      indexName: "talon",
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      replaceSearchResultPathname: {
+        from: "/docs/", // or as RegExp: /\/docs\//
+        to: "/",
+      },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: "search",
+    },
+
     docs: {
       sidebar: {
         hideable: true,
