c8ed8c552dfcf22501eeb12aba56f514736a1dec
Colton <70598503+C-Loftus@users.noreply.github.com>
2024-05-28 11:49:49 -0400

    Try new algolia config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -52,6 +52,18 @@ const config: Config = {
     // Replace with your project's social card
     image: "img/talon-community-logo-social.png",
 
+    algolia: {
+      // The application ID provided by Algolia
+      appId: 'H33NOZZCSH',
+
+      // Public API key: it is safe to commit it
+      apiKey: 'dffe48cba92d4417efcc8f3cf8f40c49',
+
+      indexName: 'talon',
+
+    },
+
+
     docs: {
       sidebar: {
         hideable: true,
