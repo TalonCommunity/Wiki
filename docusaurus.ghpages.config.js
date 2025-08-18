ceb95f12ed1c7893b76b2374e61e1ce338316aa3
2shea <yomilly@gmail.com>
2025-08-18 10:35:19 -0700

    update url in github docusaurus to reference the main site

--- docusaurus.ghpages.config.js
+++ docusaurus.ghpages.config.js
@@ -8,7 +8,7 @@ const config = {
   favicon: "img/logos/talon-community-logo.png",
 
   // Set the production url of your site here
-  url: "https://talonwikitest.com",
+  url: "https://talon.wiki",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
